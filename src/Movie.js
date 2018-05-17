626bed0b2473f8907c6757d5818a1dca0875b496
Stefen Hasselberg <stefen74@gmail.com>
2018-05-17 23:12:56 +1200

    finish course

--- src/Movie.js
+++ src/Movie.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import Overdrive from "react-overdrive";
 
-const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
+const POSTER_PATH = "http://image.tmdb.org/t/p/w154";
 
 const Movie = ({ movie }) => (
-  <div>
-    <Link to={`/${movie.id}`}>
-      <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
-    </Link>
-    <h3>{movie.title}</h3>
-  </div>
+  <Link to={`/${movie.id}`}>
+    <Overdrive id={movie.id}>
+      <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+    </Overdrive>
+  </Link>
 );
-
 
 Movie.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    desc: PropTypes.string,
-  }).isRequired,
+    desc: PropTypes.string
+  }).isRequired
 };
+
+export const Poster = styled.img`
+  box-shadow: 0 0 35px black;
+`;
 
 export default Movie;
--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -1,42 +1,77 @@
 /* eslint react/no-did-mount-set-state: 0 */
 // eslint-disable-line react/no-did-mount-set-state
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Poster } from "./Movie";
+import styled from "styled-components";
+import Overdrive from "react-overdrive";
 
-const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
-const BACKDROP_PATH = 'http://image.tmdb.org/t/p/w1280';
+const POSTER_PATH = "http://image.tmdb.org/t/p/w154";
+const BACKDROP_PATH = "http://image.tmdb.org/t/p/w1280";
 
 class MovieDetail extends Component {
   state = {
-    movie: {},
+    movie: {}
   };
   async componentDidMount() {
     try {
       const res = await fetch(
-        `https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=9d973532bc2328d58fcde62ef837d918&language=en-US`);
+        `https://api.themoviedb.org/3/movie/${
+          this.props.match.params.id
+        }?api_key=9d973532bc2328d58fcde62ef837d918&language=en-US`
+      );
 
       const movie = await res.json();
       this.setState(() => ({
-        movie,
+        movie
       }));
     } catch (error) {
-      console.log('error fetching data ', error);
+      console.log("error fetching data ", error);
     }
   }
 
   render() {
     const { movie } = this.state;
+    console.log(`${BACKDROP_PATH}${movie.backdrop_path}`);
     return (
-      <div>
-        <img src={`${BACKDROP_PATH}${movie.backdrop_path}`} alt={movie.title} />
-        <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
-        <h1>{movie.title}</h1>
-        <h3>{movie.release_date}</h3>
-        <p>{movie.overview}</p>
-      </div>
+      <MovieWrapper backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`}>
+        <MovieInfo>
+          <Overdrive id={movie.id}>
+            <Poster
+              src={`${POSTER_PATH}${movie.poster_path}`}
+              alt={movie.title}
+            />
+          </Overdrive>
+          <div>
+            <h1>{movie.title}</h1>
+            <h3>{movie.release_date}</h3>
+            <p>{movie.overview}</p>
+          </div>
+        </MovieInfo>
+      </MovieWrapper>
     );
   }
 }
 
 export default MovieDetail;
 
+const MovieWrapper = styled.div`
+  position: relative;
+  padding-top: 50vh;
+  background: url(${props => props.backdrop}) no-repeat;
+  background-size: cover;
+`;
+
+const MovieInfo = styled.div`
+  background: white;
+  text-align: left;
+  padding: 2rem 10%;
+  display: flex;
+  > div {
+    margin-left: 20px;
+  }
+  img {
+    position: relative;
+    top: -5rem;
+  }
+`;
