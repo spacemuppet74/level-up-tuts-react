93baaa3950dfac9af254cb352953bb4f73d06663
Stefen Hasselberg <stefen74@gmail.com>
2018-05-17 17:31:30 +1200

    working through style components

--- src/Movie.js
+++ src/Movie.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
 
